c0b7ff2b37c25480e5dac65e23967e371793d79d
USER <user@USERs-MacBook-Pro.local>
2024-09-17 03:09:14 -0400

    Initial commit for tenancy landing page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Home from "./components/Home/Home";
+import './Main.scss';
+function App() {
+  return (
+    <section className="app">
+      <Home />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Home.module.scss";
+const Home = () => {
+  return (
+    <section className={classes.home}>
+      <header className={classes.header}></header>
+    </section>
+  );
+};
+
+export default Home;
